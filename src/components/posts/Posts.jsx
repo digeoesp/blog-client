4e3676e0b26190fcc0e9494d114248477f1e4e6a
digeoesp <diegocrypto@outlook.com>
2021-07-29 14:12:51 -0700

    deploy

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -2,11 +2,14 @@ import Post from "../post/Post";
 import "./posts.css";
 
 export default function Posts({ posts }) {
+  const listOfPost = <div className="posts" >
+  {posts.map((p,index) => (
+    <Post post={p} key={index} />
+  ))}
+</div>
   return (
     <div className="posts" >
-      {posts.map((p,index) => (
-        <Post post={p} key={index} />
-      ))}
+      {listOfPost}
     </div>
   );
 }
