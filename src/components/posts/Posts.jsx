76f93ae9f731483fbaad44c5dd855066965a43b3
digeoesp <diegocrypto@outlook.com>
2021-08-03 17:24:37 -0700

    changed map in posts

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -2,18 +2,11 @@ import Post from "../post/Post";
 import "./posts.css";
 
 export default function Posts({ posts }) {
-//   const arrOfPost = posts.map((p,index) => {
-//     return(
-//     <Post post={[p]} key={index} />
-    
-//   )
-// })
-
   return (
-    <div className="posts" >
-      {/* {arrOfPost} */}
-      <h1>hello</h1>
+    <div className="posts">
+      {posts.map((p, index) => (
+        <Post post={p} key={index} />
+      ))}
     </div>
   );
-  
 }
